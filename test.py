45c1466228f9315758d6924ac8865509481c308a
Robert Lowder <rlowder94@gmail.com>
2023-05-26 12:15:08 -0700

    initial commit

---
+++ test.py
@@ -0,0 +1,17 @@
+"""
+Following this tutorial: https://www.youtube.com/watch?v=6W6iY7uUu34
+
+
+git add test.py
+git commit -m "initial commit"
+git push origin main
+
+
+
+
+"""
+
+import yfinance as yf
+
+ticker = yf.Ticker("VOO")
+print(ticker.info)
