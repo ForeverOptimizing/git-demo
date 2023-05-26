05a101a11b2adb79fe5f90e90a376c1497c431e8
Robert Lowder <rlowder94@gmail.com>
2023-05-26 12:25:17 -0700

    Formatting

--- test.py
+++ test.py
@@ -1,16 +1,14 @@
 """
 Following this tutorial: https://www.youtube.com/watch?v=6W6iY7uUu34
 
-How to update repo
+How to update repo:
 git add test.py
 git commit -m "initial commit"
 git push origin main
 
-
-How to make requirements.txt
+How to make requirements.txt:
 pip list
 pip freeze > requirements.txt
-
 """
 
 import yfinance as yf
